5c030956a8556e16bc28f071caaa224948767c57
Nadeem <nadeem@cuddlynest.com>
2021-06-19 14:41:05 +0500

    changed info.plist

--- ios/WeatherApp/AppDelegate.m
+++ ios/WeatherApp/AppDelegate.m
@@ -27,7 +27,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyBRni8EtDBhIW0NcA3E7nmJE7UVFNM0-nE"];
+  [GMSServices provideAPIKey:@""];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
